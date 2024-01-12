865195f8778bf6844856ae31769fbefd350aaa46
eugreg <daiaegregory@gmail.com>
2024-01-11 22:23:00 -0300

    enviando mensagens e apagando no input

--- main.py
+++ main.py
@@ -14,27 +14,37 @@ def main(pagina):
     titulo = ft.Text("GregZap")
     nome_usuario = ft.TextField(label="Escreva seu nome")
     campo_mensagem = ft.TextField(label="Digite uma mensagem")
-    botao_enviar = ft.ElevatedButton("Enviar")
+    chat = ft.Column()
 
 
-    def entrar_popup(evento):
+
+    def enviar_mensagem(evento):
+        chat.controls.append(ft.Text(f"{nome_usuario.value}: { campo_mensagem.value}"))
+        # limpar o campo de mensagem
+        campo_mensagem.value = ""
+        pagina.update()
+
+    botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+    
+    def entrar_chat(evento):
         popup.open = False
         pagina.remove(botao_iniciar)
         linha = ft.Row(
             [campo_mensagem, botao_enviar]
         )
+        pagina.add(chat)
         pagina.add(linha)
         pagina.update()
         
 
-    popup = ft.AlertDialog(title=ft.Text("Digite seu nome"), content=nome_usuario,  actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)], open=False, )
+    popup = ft.AlertDialog(title=ft.Text("Digite seu nome"), content=nome_usuario,  actions=[ft.ElevatedButton("Entrar", on_click=entrar_chat)], open=False, )
 
-    def entrar_chat(evento):
+    def entrar_popup(evento):
         popup.open = True
         pagina.dialog = popup
         pagina.update()
 
-    botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
+    botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_popup)
 
 
     pagina.add(titulo)
